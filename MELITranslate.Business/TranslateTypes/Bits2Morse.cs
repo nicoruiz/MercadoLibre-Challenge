ebd01c027296c5fa32d15c84164706fc007e4fbb
nicoruiz <nicoruiz182@gmail.com>
2020-01-17 23:22:05 -0300

    Binary to Morse decode method and tests added

---
+++ MELITranslate.Business/TranslateTypes/Bits2Morse.cs
@@ -0,0 +1,44 @@
+﻿using MELITranslate.Business.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MELITranslate.Business.TranslateTypes
+{
+    public class Bits2Morse : ITranslateType
+    {
+        public string Translate(string value)
+        {
+            string result = String.Empty;
+
+            for(int i=0; i < value.Length; i+=2)
+            {
+                result += GetMorseEquivalent(value.Substring(i, 2));
+            }
+            return result;
+        }
+
+        private string GetMorseEquivalent(string binValue)
+        {
+            string morseValue = String.Empty;
+            switch(binValue)
+            {
+                case "10":
+                    morseValue = ".";
+                    break;
+                case "11":
+                    morseValue = "-";
+                    break;
+                case "01":
+                    morseValue = " ";
+                    break;
+                case "00":
+                    morseValue = "  ";
+                    break;
+                default:
+                    break;
+            }
+            return morseValue;
+        }
+    }
+}
