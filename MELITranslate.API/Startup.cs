5cbc4043d0d5c82324144fdcd5db6d0a938273ee
nicoruiz <nicoruiz182@gmail.com>
2020-01-23 11:36:26 -0300

    Dependency added to publish to azure

--- MELITranslate.API/Startup.cs
+++ MELITranslate.API/Startup.cs
@@ -1,18 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using MELITranslate.Business;
-using MELITranslate.Business.Interfaces;
 using MELITranslate.Business.TranslateStrategies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi;
 using Microsoft.OpenApi.Models;
 
 namespace MELITranslate.API
