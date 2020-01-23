55618a53c411236c1076221d2c0e8b09e64aeab1
nicoruiz <nicoruiz182@gmail.com>
2020-01-23 11:19:44 -0300

    Swagger added

--- MELITranslate.API/Startup.cs
+++ MELITranslate.API/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace MELITranslate.API
 {
@@ -32,6 +33,11 @@ namespace MELITranslate.API
             services.AddScoped<Bits2MorseStrategy>();
             services.AddScoped<Morse2TextStrategy>();
             services.AddScoped<Text2MorseStragety>();
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MeLi Translate API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +50,15 @@ namespace MELITranslate.API
 
             app.UseHttpsRedirection();
 
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MeLi Translate API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
             app.UseRouting();
 
             app.UseAuthorization();
@@ -51,7 +66,7 @@ namespace MELITranslate.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-            });
+            });            
         }
     }
 }
